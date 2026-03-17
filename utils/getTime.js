397e9d35b9143748ffcaedb4fd26dbd61e7ea784
Yusuf <yb8952464@gmail.com>
2026-03-17 01:21:57 -1100

    fix nigeria time

--- utils/getTime.js
+++ utils/getTime.js
@@ -1,9 +1,12 @@
 function getTime() {
-  return new Date().toLocaleTimeString("en-NG", {
+  return new Date().toLocaleString("en-NG", {
     timeZone: "Africa/Lagos",
     hour: "2-digit",
     minute: "2-digit",
-    second: "2-digit"
+    second: "2-digit",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
   });
 }
 
